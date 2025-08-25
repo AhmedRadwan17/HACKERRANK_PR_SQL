7237531c66ea0c0eb2c39f994648047f340db066
Ahmed Radwan <marimradwan922@gmail.com>
2025-08-26 01:05:17 +0300

    The PADS.sql

---
+++ ADVANCED_SELECT/The PADS.sql
@@ -0,0 +1,5 @@
+select concat(name,'(', left(Occupation,1),')')
+from occupations order by name asc;
+
+select concat('There are a total of ',count(*),' ',lower(occupation),'s.') from occupations group by occupation order by count(*) asc;
+
