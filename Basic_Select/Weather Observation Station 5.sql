1ffed727c3c762510e0387a29c864500440f6974
Ahmed Radwan <marimradwan922@gmail.com>
2025-09-10 18:09:23 +0300

    Weather Observation Station 5.sql

--- Basic_Select/Weather Observation Station 5.sql
+++ Basic_Select/Weather Observation Station 5.sql
@@ -3,3 +3,17 @@ select TOP 1 (city) , len(city)from station where len(city) = (select max(len(ci
 select TOP 1  (city) , len(city)from station where len(city) = (select min(len(city)) from station)  
 order by city asc , len(city)  asc  ;
 
+
+--> another solve
+
+(
+    SELECT TOP 1 CITY, LEN(CITY) AS NAME_LENGTH
+    FROM STATION
+    ORDER BY LEN(CITY) ASC, CITY ASC
+)
+UNION
+(
+    SELECT TOP 1 CITY, LEN(CITY) AS NAME_LENGTH
+    FROM STATION
+    ORDER BY LEN(CITY) DESC, CITY ASC
+);
