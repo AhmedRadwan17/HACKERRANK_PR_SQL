4c77acd6bae96172de2f6bdeac3d37565e45f4cd
Ahmed Radwan <marimradwan922@gmail.com>
2026-02-21 16:39:02 +0200

    The PADS.sql

--- ADVANCED_SELECT/The PADS.sql
+++ ADVANCED_SELECT/The PADS.sql
@@ -3,3 +3,12 @@ from occupations order by name asc;
 
 select concat('There are a total of ',count(*),' ',lower(occupation),'s.') from occupations group by occupation order by count(*) asc;
 
+-- another solve
+select concat(name,'(',left(OCCUPATION,1),')') 
+from OCCUPATIONS 
+order by name
+
+select concat('There are a total of ',count(lower(OCCUPATION)),' ',lower(OCCUPATION),'s','.')
+from OCCUPATIONS 
+group by OCCUPATION
+ORDER BY COUNT(occupation) ASC, occupation ASC 
